0413cbd377f6d7d3a4da1ebea94a57c520a3e8e5
thomasdinh <thomas.dinh.work@outlook.de>
2025-02-01 15:09:49 +0100

    winrate evaluation start dev

--- backend_calc.py
+++ backend_calc.py
@@ -1,18 +1,22 @@
 import pandas as pd
 import numpy as np
 
-default_match_path = "./match_data/match_data.csv"
+default_match_log_path = "./match_data/match_data.csv"
+match_df = []
 
-def read_match_logs():
+def read_match_logs(filepath = None):
+    csv_filepath = default_match_log_path if filepath is None else filepath
     print("Reading matchlogs")
     try:
-        with open ('./match_data/match_data.csv','r') as match_log_file:
-            content = match_log_file.read()
-            print(content)
+        match_df = pd.read_csv(csv_filepath)
+        print(match_df)
     except FileNotFoundError:
-        print("file not found")
+        print("file not found: match_data.csv is missing")
 
     return 0
+
+def top_deck_wr(ranks = 1, exclude_draw = False):
+    return
 
 if __name__ == "__main__":
     read_match_logs()
