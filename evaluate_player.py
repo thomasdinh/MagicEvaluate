4b3da60b068ec8868c7e6468961ac3c958958615
PeterGundam <mob0001@web.de>
2025-02-01 15:40:28 +0100

    csv_readfile

--- evaluate_player.py
+++ evaluate_player.py
@@ -1,8 +1,23 @@
-def hello_world():
-    print("hello world!")
+import pandas as pd
+
+
+default_path = "./player_data/player_data.csv"
+
+def read_csv(filepath = None):
+    csv_filepath = filepath
+    print("Reading matchlogs")
+    try:
+        csv_df = pd.read_csv(csv_filepath)
+        #print(csv_df)
+    except FileNotFoundError:
+        print(f"file not found: {filepath}.csv is missing")
+
+    return csv_df
 
 
 
 
 if __name__ == "__main__":
-   hello_world()
+   print(read_csv(default_path))
+
+   
