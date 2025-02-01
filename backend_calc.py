b0852e708d9ba3056b043ca4e0ca2ec1bfaf5b9f
thomasdinh <thomas.dinh.work@outlook.de>
2025-02-01 20:09:02 +0100

    added top_deck_wr()

--- backend_calc.py
+++ backend_calc.py
@@ -2,21 +2,26 @@ import pandas as pd
 import numpy as np
 
 default_match_log_path = "./match_data/match_data.csv"
-match_df = []
+
 
 def read_match_logs(filepath = None):
     csv_filepath = default_match_log_path if filepath is None else filepath
-    print("Reading matchlogs")
+    #print("Reading matchlogs")
     try:
         match_df = pd.read_csv(csv_filepath)
-        print(match_df)
+        #clearprint(match_df)
     except FileNotFoundError:
         print("file not found: match_data.csv is missing")
 
-    return 0
+    return match_df
 
 def top_deck_wr(ranks = 1, exclude_draw = False):
-    return
+    match_df = read_match_logs()
+    selected_col = ['Decklist','match_result']
+    match_results = match_df[selected_col]
+    first_data = match_results.values[0][0]
+    print(first_data)
+    return 0
 
 if __name__ == "__main__":
-    read_match_logs()
+    top_deck_wr()
