5b50ef0cd7d0e569715bf59caa4be96b499d23d2
thomasdinh <thomas.dinh.work@outlook.de>
2025-03-24 19:12:06 +0100

    css for sortpicker

--- frontend/src/components/SortPicker.jsx
+++ frontend/src/components/SortPicker.jsx
@@ -1,8 +1,10 @@
+import "../css/sort-picker.css"
+
 function SortPicker({ onChange }){
     return (
-        <label>
+        <label className="sortLabel">
           Sort Order:  
-          <select name="sortOrder" onChange={onChange}>
+          <select name="sortOrder" className="sort-picker" onChange={onChange}>
             <option value="name_asc">Name Asc.  (A-Z)</option>
             <option value="name_desc">Name Desc. (Z-A)</option>
             <option value="asc_winrate">Winrate Asc. (0-100)</option>
